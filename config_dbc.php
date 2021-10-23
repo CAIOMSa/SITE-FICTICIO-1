8a0fbf8b98ecd4491c340b6b3d3730abaa3d0379
caio <86206708+C410M@users.noreply.github.com>
2021-10-23 18:57:36 -0300

    Create config_dbc.php

---
+++ config_dbc.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'elektro';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
